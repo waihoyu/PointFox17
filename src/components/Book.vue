f349eb4bae429435efb7bffa8aa160811ed4aff8
waihoyu <13701343809@qq.com>
2021-05-18 08:53:56 +0800

    前端项目初始化

---
+++ src/components/Book.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="bookWrapper">    
+      <div id="title">
+        图书库存情况
+      </div>
+      <div id="bookDetail">
+      </div>
+  </div>
+</template>
+
+<script>
+import * as echarts from "echarts";
+
+export default {
+  name:"book",
+  mounted() {
+    var myChart = echarts.init(document.getElementById("bookDetail"));
+    // 绘制图表
+    myChart.setOption({
+      title: {
+        text: "ECharts 入门示例",
+      },
+      tooltip: {},
+      xAxis: {
+        data: ["Java", "Python", "C++", "C", "Node.js", "Vue.js"],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "销量",
+          type: "bar",
+          data: [5, 20, 36, 10, 10, 20],
+        },
+      ],
+    });
+  },
+
+}
+</script>
+
+<style lang="stylus">
+#bookWrapper
+  background-color:red
+  height:500px
+  #title
+    background-color: blue
+    height:200px
+  #bookDetail
+    height: 300px
+</style>
---
+++ src/components/Reader.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="readerWrapper">
+    <div id="title">
+      <span>我是读者模块</span>
+    </div>
+    <div id="myChart"></div>
+  </div>
+</template>
+
+<script>
+import * as echarts from "echarts";
+export default {
+  name: "reader",
+  mounted() {
+    var myChart = echarts.init(document.getElementById("myChart"));
+    // 绘制图表
+    myChart.setOption({
+      title: {
+        text: "ECharts 入门示例",
+      },
+      tooltip: {},
+      xAxis: {
+        data: ["Java", "Python", "C++", "C", "Node.js", "Vue.js"],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "销量",
+          type: "bar",
+          data: [5, 20, 36, 10, 10, 20],
+        },
+      ],
+    });
+  },
+  methods: {},
+};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+#readerWrapper {
+  // background-color: red;
+  height: 500px;
+
+  #title {
+    background-color: blue;
+    height: 100px;
+  }
+  #myChart{
+    height: 300px;
+  }
+}
+</style>
