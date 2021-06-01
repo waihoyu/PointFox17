4bb5cc53e0d3729af44a91eb60ed76690d29d72f
waihoyu <13701343809@qq.com>
2021-06-01 09:21:05 +0800

    '完善用户注册'

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -4,15 +4,34 @@
       <el-card class="box-card">
         <div slot="header" class="clearfix">
           <span>用户注册</span>
-          <span id="checkTip" v-text="checktip" :class="flag?tipRed:tipGreen"></span>
+          <span
+            id="checkTip"
+            v-text="checktip"
+            :class="flag ? tipRed : tipGreen"
+          ></span>
         </div>
         <div id="registerContentBody">
-            <el-input id="newusername" v-model="newUser.newusername" clearable @blur="checkUsername"></el-input>
-            <el-input id="newpassword" v-model="newUser.newpassword" show-password clearable></el-input>
+          <el-input
+            id="newusername"
+            v-model="newUser.newusername"
+            clearable
+            @blur="checkUsername"
+          ></el-input>
+          <el-input
+            id="newpassword"
+            v-model="newUser.newpassword"
+            show-password
+            clearable
+          ></el-input>
         </div>
         <!--  v-loading.fullscreen.lock="fullscreenLoading" -->
         <div id="registerSubmit">
-            <el-button id="systemSubmit" @click="registerSystem" >注册</el-button>
+          <el-button
+            id="systemSubmit"
+            @click="registerSystem"
+            :disabled="this.disabled"
+            >注册</el-button
+          >
         </div>
       </el-card>
     </div>
@@ -26,6 +45,7 @@ export default {
   name:"register",
     data(){
         return {
+            disabled:true,
             newUser:{
                 newusername:"",
                 newpassword:""
@@ -42,11 +62,13 @@ export default {
         Axios.post(url + '/register/check',{
           params:this.newUser
         }).then((response)=>{
-          // console.log(response)
-          if(response.data.state =="success"){
+          console.log(response)
+          if(response.data.state =="success"){            
             this.flag = false
+            this.disabled = false          
           }else{
             this.flag = true
+            this.disabled =true
           }
           this.checktip = response.data.message
             // if(response.data.state =="success"){
@@ -58,50 +80,53 @@ export default {
         })
       },
       // CORS 
-      registerSystem(){     
-        // Axios.get(url +'/register',{
-        //   params: this.newUser
-        // }).then(function(response){
-        //   alert(response.data.state + '\n' +response.data.message )
-        // })
+      registerSystem(){  
+        Axios.post(url +'/register',{
+          params: this.newUser
+        }).then((response)=>{
+        if(response.data.state == "success"){
+          console.log(response.data.state)
+          this.$message("注册成功")  
+          this.newUser.newusername=""
+          this.newUser.newpassword=""
+          this.checktip="" 
+        }})
         // this.fullscreenLoading = true;
         // that = this
-
       }
     }
 };
 </script>
 
 <style >
-
 #registerWrapper {
   /* background-color: purple; */
   height: 100%;
 }
-#registerContent{
-    width: 400px;
-    margin:0 auto;
-    padding-top: 100px;
+#registerContent {
+  width: 400px;
+  margin: 0 auto;
+  padding-top: 100px;
 }
-#systemSubmit{
-    display: block;
-    margin: 0 auto;
-    background-color: #0a9588;
-    color:white;
-    border-color: #0a9588;
+#systemSubmit {
+  display: block;
+  margin: 0 auto;
+  background-color: #0a9588;
+  color: white;
+  border-color: #0a9588;
 }
-#newpassword{
-  margin-top:5px;
+#newpassword {
+  margin-top: 5px;
   margin-bottom: 8px;
 }
-#checkTip{
+#checkTip {
   /* background-color: red; */
   margin-left: 50px;
 }
-.tipRed{
-  color:red;
+.tipRed {
+  color: red;
 }
-.tipGreen{
-  color:green;
+.tipGreen {
+  color: green;
 }
 </style>
